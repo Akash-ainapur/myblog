77aed5146a7e8587f61b8682a16e35911827c14c
Akash-ainapur <akashainapur4548@gmail.com>
2025-03-04 19:19:29 +0530

    finally hopefully

--- generateblog.py
+++ generateblog.py
@@ -47,7 +47,7 @@ def updateindex(postfiles,output_dire):
         post_path = os.path.splitext(os.path.basename(f))[0] + '.html'
         with open(f,'r') as fl:
             title = fl.readline().strip()
-            links += f'<li><a href = "{post_path}" >{title}</a></li>'
+            links += f'<li><a href = "//output + {post_path}" >{title}</a></li>'
         
     content = INDEX_TEMPLATE.format(link = links)
     with open(output_dire,'w') as f:
